482f66ab73e5d39dae3d31e3578747af0b6d1794
Omar Saleem <omaroidd@gmail.com>
2025-12-10 18:54:07 +0300

    Revert "setup project + modules"
    This reverts commit e9ec5ba41dcec4564328c8173b9260e41574a505.

--- app/src/main/java/com/example/foodicsmenutask/domain/model/OrderItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.foodicsmenutask.domain.model
-
-data class OrderItem(
-    val productId: Int,
-    val productName: String,
-    val price: Double,
-    val quantity: Int,
-    val image: String
-)
